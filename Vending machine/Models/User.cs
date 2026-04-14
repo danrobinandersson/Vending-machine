4425b703cd6bf8bcd1aecfcec05fc762654ee5d9
Robin Andersson <danrobin.andersson@outlook.com>
2026-04-14 08:21:08 +0200

    Added user class

--- Vending machine/Models/User.cs
+++ Vending machine/Models/User.cs
@@ -1,6 +1,29 @@
 namespace Vending_machine.Models;
 
-public class User
+class User
 {
-    
+    public string Name { get; set; }
+    public int Money { get; private set; }
+    public List<Product> BoughtItems { get; set; } = new List<Product>();
+
+    public User(string name, int money)
+    {
+        Name = name;
+        Money = money;
+    }
+
+    public bool SpendMoney(int amount)
+    {
+        if (Money >= amount)
+        {
+            Money -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void AddItem(Product product)
+    {
+        BoughtItems.Add(product);
+    }
 }
